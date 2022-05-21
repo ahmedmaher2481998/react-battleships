04424fb1501261b94addf9ab57ee2a831ac270ed
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-05-21 14:52:31 +0200

    stlye:grid componeted style complete

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Grid = (props) => {
 	return (
 		<>
-			<div>this is grid</div>
+			<div
+				className='w-2/3 h-2/3  text-white bg-transparent border-4 rounded-lg border-gray-900 border-opacity-70
+			focus:grid-shadow hover:grid-shadow active:grid-shadow
+			outline-none  border-solid border- flex p-1 '
+			>
+				{props.children}
+			</div>
 		</>
 	);
 };
--- src/components/GridWrapper.jsx
+++ src/components/GridWrapper.jsx
@@ -4,7 +4,13 @@ import Grid from "./Grid";
 import Row from "./Row";
 const GridWrapper = () => {
 	return (
-		<div className='w-screen h-screen grid-shadow flex justify-center items-center bg-gray-100 border-2 border-x-2 border-solid border-gray-800  '>
+		<div
+			className="w-screen h-screen flex-col flex justify-center items-center z-0 
+        bg-[url('../src/main.jpg')] bg-no-repeat bg-cover bg-center  bg-fixed  shadow-inner"
+		>
+			<h1 className='text-4xl text-black bg-gray-500  rounded-lg p-3 bg-opacity-50 hover:bg-opacity-70 hover:shadow-2xl -mt-30  mb-4 '>
+				Welcome To BattleShip
+			</h1>
 			<Grid>
 				<Row>
 					<Cell />
