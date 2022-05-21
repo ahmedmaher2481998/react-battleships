00fe60c114b7509be323d0db2fdb950d83896955
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-05-21 13:46:12 +0200

    setup is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Cell from "./components/Cell";
+import Grid from "./components/Grid";
+import Row from "./components/Row";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Grid>
+				<Row>
+					<Cell />
+				</Row>
+			</Grid>
+		</>
+	);
 }
 
 export default App;
