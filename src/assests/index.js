8aab3072210cd2009f2e924b5541446585ff3c3e
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-06-07 18:13:35 +0200

    fleet is in place

--- src/assests/index.js
+++ src/assests/index.js
@@ -4,5 +4,6 @@ import ship from "./ship.png";
 import explosion from "./explosion.png";
 import submarine from "./submarine.png";
 import heroVideo from "./herovideo.mp4";
+import carrier from "./Carrier.png";
 //exporting assests for use
-export { heroVideo, battlesShip, ship, submarine, boat, explosion };
+export { heroVideo, battlesShip, ship, submarine, boat, explosion, carrier };
