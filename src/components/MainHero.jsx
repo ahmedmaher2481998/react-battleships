2664c58521d296f3e81d9a2dd373bb0528c8eacc
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-06-09 16:27:04 +0200

    fixed view bugs in the welcome page

--- src/components/MainHero.jsx
+++ src/components/MainHero.jsx
@@ -22,8 +22,7 @@ const MainHero = () => {
 	return (
 		<main
 			className=' 
-    bg-mainmeduimblue w-screen lg:h-[var(--contentHeight)]
-	h-full
+     bg-transparent w-screen h-[var(--contentHeight)]
     flex flex-col justify-center items-center p-8 
     '
 		>
@@ -31,38 +30,48 @@ const MainHero = () => {
 			<h1 className='mb-auto hover:text-bage text-mainheavyblue font-bold tracking-wide text-4xl'>
 				Welcome to BattleShip{" "}
 			</h1>
-
-			<video frameBorder='0' autoPlay muted loop>
-				<source
-					className='w-4/6 h-3/6'
-					type='video/mp4'
-					src={heroVideo}
-				/>
-			</video>
-
-			<div>
-				<label className='label' htmlFor='name'>
-					Please enter Your name
-				</label>
-				<input
-					className='inputtext'
-					type='text'
-					id='name'
-					value={name}
-					placeholder='Your Name ? '
-					onChange={(e) => {
-						changeName(e.target.value);
-					}}
-				/>
+			<div className='w-screen  h-screen -z-10 absolute'>
+				<video
+					frameBorder='0'
+					className='relative top-0 left-0 w-screen h-screen object-cover '
+					autoPlay
+					muted
+					loop
+				>
+					<source type='video/mp4' src={heroVideo} />
+				</video>
 			</div>
-			<span>
-				<button onClick={(e) => setName(e, false)} className='btn m-4'>
-					Read Rules
-				</button>
-				<button onClick={(e) => setName(e, true)} className='btn mt-4'>
-					Start Game
-				</button>
-			</span>
+			<div className=' mb-auto'>
+				<div>
+					<label className='label' htmlFor='name'>
+						Please enter Your name
+					</label>
+					<input
+						className='inputtext'
+						type='text'
+						id='name'
+						value={name}
+						placeholder='Your Name ? '
+						onChange={(e) => {
+							changeName(e.target.value);
+						}}
+					/>
+				</div>
+				<span className='flex flex-col lg:flex-row justify-center mt-2 lg:mt-6 items-center '>
+					<button
+						onClick={(e) => setName(e, false)}
+						className='btn m-2 w-28 h-18 lg:w-32 lg:h-22'
+					>
+						Read Rules
+					</button>
+					<button
+						onClick={(e) => setName(e, true)}
+						className='btn m-2  w-28 h-18 lg:w-32 lg:h-22'
+					>
+						Start Game
+					</button>
+				</span>
+			</div>
 		</main>
 	);
 };
