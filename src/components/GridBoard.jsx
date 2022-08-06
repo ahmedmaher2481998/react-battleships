32df8761c0fc7f556c415f41c8ba7f89fe031705
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-08-06 20:44:41 +0200

    inprogress : grid recognize if it's a pc or player

--- src/components/GridBoard.jsx
+++ src/components/GridBoard.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect } from 'react';
 import Cell from './Cell';
 import Row from './Row';
-import { generateCellId ,validateShipLocation} from '../helpers';
+import { generateCellId, validateShipLocation } from '../helpers';
 import { initCellState } from '../store/cells/cellReducer';
 import { ROW_SIZE } from '../helpers';
 import {
   getSelectedShip,
   getPlacingStatus,
   initCells,
+  occupyCell,
   // changeGameState,
 } from '../store';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 //end of imports
-const createGrid = (size) => {
+const createGrid = (size, pc = false) => {
   let rows = [];
   let cellsState = {};
   //creating rows
@@ -22,59 +24,89 @@ const createGrid = (size) => {
     //creating cells
     for (let col = 1; col < size + 1; col++) {
       //creating grid
-      cells.push(<Cell key={generateCellId(row, col)} row={row} col={col} />);
+      cells.push(
+        <Cell key={generateCellId(row, col)} row={row} col={col} pc={pc} />
+      );
       //creating cells state
       cellsState[`${generateCellId(row, col)}`] = initCellState(row, col);
     }
-    rows.push(<Row key={row} cells={cells} row={row} />);
+    rows.push(<Row key={row} cells={cells} />);
   }
-
   return { cellsState, rows };
 };
 
 const GridBoard = ({ pc }) => {
   pc = pc || false;
+
   const dispatch = useDispatch();
+
   const { pathname } = useLocation();
-  // const { rows, cellsState } = createGrid(10);
+
   const selectedShip = useSelector((s) => getSelectedShip(s));
+
   const placingStatus = useSelector((s) => getPlacingStatus(s));
 
-  const { rows, cellsState } = createGrid(ROW_SIZE);
+  const [pcCells, setPcCells] = useState([]);
+
+  const { rows, cellsState } = createGrid(ROW_SIZE, pc ? pc : false);
 
   useEffect(() => {
-    dispatch(initCells(cellsState));
+    if (!pc) {
+      dispatch(initCells(cellsState));
+    } else {
+      setPcCells(cellsState);
+    }
     //eslint-disable-next-line
   }, []);
+
   //the pc is the on generating the grid
   if (pc) {
-    const getRandom = ()=>Math.floor((Math.random() * 10) +1 )
-   const getPlacingPosition = ({ row, col }) => {
-  //improving the chances of placing the ship
-  if (row > 5) return 'H';
-  else if (col > 5) return 'V';
-  else if (col > row) return 'V';
-  //means it's better chance in the h direction
-  else if (row > col) return 'H';
-  else return 'V';
-};
+    const getRandom = () => Math.floor(Math.random() * 10 + 1);
+
+    const getPlacingPosition = ({ row, col }) => {
+      //improving the chances of placing the ship
+      if (row > 5) return 'H';
+      else if (col > 5) return 'V';
+      else if (col > row) return 'V';
+      //means it's better chance in the h direction
+      else if (row > col) return 'H';
+      else return 'V';
+    };
 
     const fleet = {
       ship: ['boat', 'ship', 'battlesShip', 'submarine', 'carrier'],
       shipSize: {
         boat: 1,
         ship: 2,
-        battlesShip: 4,
         submarine: 3,
+        battlesShip: 4,
         carrier: 5,
       },
     };
-    fleet.ship.forEach(shipName=>{
-      let randomCol = getRandom()
-      let randomRow =  getRandom()
-      while(!validateShipLocation({row:randomRow,col:randomCol,placingPosition: getPlacingPosition(row:randomRow,col:randomCol) }))
-    })
+
+    fleet.ship.forEach((shipName) => {
+      let randomCol = getRandom();
+
+      let randomRow = getRandom();
+
+      while (
+        !validateShipLocation({
+          row: randomRow,
+          col: randomCol,
+          placingPosition: getPlacingPosition({
+            row: randomRow,
+            col: randomCol,
+          }),
+          cells: '',
+          isOccupied:
+            pcCells[generateCellId(randomRow, randomCol)]?.occupyCell
+              .isOccupied,
+          shipSize: fleet.shipSize[shipName],
+        })
+      ) {}
+    });
   }
+
   return (
     <div
       className={`${
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,14 +1,13 @@
-import React from "react";
+import React from 'react';
 
-const Row = ({ row, cells }) => {
-	return (
-		<>
-			{/* <div data-row={`${row}`}				className=' h-[9%] w-[100%] flex  rounded-lg justify-between'			> */}
-			<div className=' h-[9%] w-[100%] flex  rounded-lg justify-between'>
-				{cells}
-			</div>
-		</>
-	);
+const Row = ({ cells }) => {
+  return (
+    <>
+      <div className=" h-[9%] w-[100%] flex  rounded-lg justify-between">
+        {cells}
+      </div>
+    </>
+  );
 };
 
 export default Row;
