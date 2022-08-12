bf42f85a57a0412cd4e4a7d5e7910a08ce0e9908
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-08-12 05:49:19 +0200

    style : sm/md/lg layout of wiinner  paged

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,15 @@ import { FaFacebook, FaTwitter, FaReddit, FaGithub } from 'react-icons/fa';
 import { useLocation } from 'react-router-dom';
 function Footer() {
   const { pathname } = useLocation();
+
   return (
     <>
       <div
         id="footer"
         className={`${
           pathname === '/battle' ? 'bg-mainred' : 'bg-mainblue'
-        } max-w-screen min-w-fit text-bage selection:text-xl flex flex-wrap : ;
+        }'text-bage'
+        max-w-screen min-w-fit  selection:text-xl flex flex-wrap : ;
 				flex-row items-center justify-center`}
       >
         <div className="h-24 p-2 gap-6 max-w-[80%] flex flex-wrap flex-row items-center justify-between">
--- src/pages/WinnerPage.js
+++ src/pages/WinnerPage.js
@@ -1,7 +1,6 @@
-import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FaSmileBeam, FaRegSadTear } from 'react-icons/fa/index';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { getBotResult, getName, getPlayerResult } from '../store';
 import { victory } from '../assets';
 import { getDurationInMinutes } from '../helpers';
@@ -12,10 +11,13 @@ const WinnerPage = () => {
   const getWinnerScore = useSelector((s) =>
     winnerName === 'bot' ? getBotResult(s) : getPlayerResult(s)
   );
+  const getLoserScore = useSelector((s) =>
+    winnerName !== 'bot' ? getBotResult(s) : getPlayerResult(s)
+  );
   const name = useSelector(getName);
   const startTime = useSelector((s) => new Date(s.main.startTime));
-  const botResult = useSelector(getBotResult);
-  const playerResult = useSelector(getPlayerResult);
+  // const botResult = useSelector(getBotResult);
+  // const playerResult = useSelector(getPlayerResult);
   const timePlayed = `${startTime.toLocaleDateString('en-us', {
     weekday: 'long',
     year: 'numeric',
@@ -25,70 +27,67 @@ const WinnerPage = () => {
           ${startTime.toLocaleTimeString('en-US')}`;
   useEffect(() => {
     if (name === '') navigate('/');
+    //eslint-disable-next-line
   }, []);
   return (
-    <>
+    <div className="relative flex items-center justify-center min-h-[var(--contentHeight)] glass">
       <div
         style={{ backgroundImage: `url(${victory})` }}
-        className=" mx-auto h-screen w-screen bg-cover bg-center"
-      >
-        <div className="flex items-center justify-center h-full">
-          <div className="glass shadow-2xl p-6 rounded-2xl border-2 border-gray-200">
-            <div className="flex flex-col">
-              <div>
-                <h2 className="font-bold text-gray-500 text-center">
-                  The Winner Is {winnerName}
-                </h2>
-              </div>
-              <div className="my-6">
-                <div className="flex flex-row space-x-4 items-center">
-                  <div id="icon">
-                    <span
-                      className={`${
-                        winnerName === 'bot'
-                          ? 'text-rose-400'
-                          : 'text-green-400'
-                      } text-4xl`}
-                    >
-                      {winnerName === 'bot' ? (
-                        <FaRegSadTear />
-                      ) : (
-                        <FaSmileBeam />
-                      )}
-                    </span>
-                  </div>
-                  <div id="temp">
-                    <h4 className="text-4xl mb-4  text-white">
-                      Score: {getWinnerScore}
-                    </h4>
-                    <p className="text-s flex flex-wrap max-w-sm text-gray-400 lg:text-lg">
-                      the game started at {timePlayed}, and it lasted for{' '}
-                      {getDurationInMinutes(startTime)} min, it was an epic
-                      battle.which ended by winning of {winnerName} by
-                      {playerResult > botResult ? playerResult : botResult}
-                      ,great game {name}
-                    </p>
-                  </div>
+        className="
+        top-0 left-0 
+        h-screen w-screen bg-cover bg-center object-contain -z-10 absolute "
+      ></div>
+      <div className=" z-10  h-full">
+        <div className="glass shadow-2xl p-6 rounded-2xl border-2 border-gray-200">
+          <div className="flex flex-col">
+            <div>
+              <h2 className="font-bold text-gray-800 text-center">
+                The Winner Is {winnerName}
+              </h2>
+            </div>
+            <div className="my-6">
+              <div className="flex flex-row space-x-4 items-center">
+                <div id="icon">
+                  <span
+                    className={`${
+                      winnerName === 'bot' ? 'text-rose-600' : 'text-green-600'
+                    } text-4xl `}
+                  >
+                    {winnerName === 'bot' ? <FaRegSadTear /> : <FaSmileBeam />}
+                  </span>
+                </div>
+                <div id="temp">
+                  <h4 className="text-4xl mb-4  text-black">
+                    Score: {getWinnerScore} v.s {getLoserScore}
+                  </h4>
+                  <p className="text-s flex flex-wrap max-w-sm text-gray-800 lg:text-lg">
+                    the game started at {timePlayed}, and it lasted for{' '}
+                    {getDurationInMinutes(startTime)} min, it was an epic
+                    battle.which ended by winning of {winnerName} by
+                    {getWinnerScore} vs {getLoserScore}
+                    ,great game {name} , go check your result on the result
+                    board...
+                  </p>
                 </div>
               </div>
-              <div className="w-full place-items-end text-right border-t-2 border-gray-100 mt-2">
-                <button
-                  type="button"
-                  onClick={() => {
-                    navigate('/results');
-                  }}
-                  className="border border-teal-500 bg-teal-500 text-white rounded-md px-3 py-2 m-2 
+            </div>
+            <div className="w-full place-items-end text-right border-t-2 border-gray-100 mt-2">
+              <button
+                type="button"
+                onClick={() => {
+                  navigate('/results');
+                }}
+                className="border border-teal-500 bg-teal-500 text-white rounded-md px-3 py-2 m-2 
                   transition duration-500 ease select-none hover:bg-teal-600 focus:outline-none
                   focus:shadow-outline"
-                >
-                  Go to Results board
-                </button>
-              </div>
+              >
+                Go to Results board
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
